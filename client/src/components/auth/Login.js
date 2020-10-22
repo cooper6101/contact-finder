8eb8f226936ab2799faa0ac6d870b0ff050c28c1
Trey <cooper6101@gmail.com>
2020-10-22 10:48:49 -0400

    final build

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -14,7 +14,7 @@ const Login = (props) => {
             props.history.push('/')
         }
 
-        if(error === 'Invalid Credentials') {
+        if(error) {
             setAlert(error, 'danger');
             clearErrors();
         }
